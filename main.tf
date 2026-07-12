40235b76177af788ccace1da784f6dec4cbfc3a8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:24 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+data "azurerm_key_vault_secret" "agent_account_access_key" {
+  for_each     = { for k, v in var.spring_cloud_app_dynamics_application_performance_monitorings : k => v if v.agent_account_access_key_key_vault_id != null && v.agent_account_access_key_key_vault_secret_name != null }
+  name         = each.value.agent_account_access_key_key_vault_secret_name
+  key_vault_id = each.value.agent_account_access_key_key_vault_id
+}
+data "azurerm_key_vault_secret" "agent_account_name" {
+  for_each     = { for k, v in var.spring_cloud_app_dynamics_application_performance_monitorings : k => v if v.agent_account_name_key_vault_id != null && v.agent_account_name_key_vault_secret_name != null }
+  name         = each.value.agent_account_name_key_vault_secret_name
+  key_vault_id = each.value.agent_account_name_key_vault_id
+}
 resource "azurerm_spring_cloud_app_dynamics_application_performance_monitoring" "spring_cloud_app_dynamics_application_performance_monitorings" {
   for_each = var.spring_cloud_app_dynamics_application_performance_monitorings
 
-  agent_account_access_key = each.value.agent_account_access_key
-  agent_account_name       = each.value.agent_account_name
+  agent_account_access_key = each.value.agent_account_access_key != null ? each.value.agent_account_access_key : try(data.azurerm_key_vault_secret.agent_account_access_key[each.key].value, null)
+  agent_account_name       = each.value.agent_account_name != null ? each.value.agent_account_name : try(data.azurerm_key_vault_secret.agent_account_name[each.key].value, null)
   controller_host_name     = each.value.controller_host_name
   name                     = each.value.name
   spring_cloud_service_id  = each.value.spring_cloud_service_id
--- variables.tf
+++ variables.tf
@@ -3,7 +3,11 @@ variable "spring_cloud_app_dynamics_application_performance_monitorings" {
 Map of spring_cloud_app_dynamics_application_performance_monitorings, attributes below
 Required:
     - agent_account_access_key
+    - agent_account_access_key_key_vault_id (alternative to agent_account_access_key - read from Key Vault instead)
+    - agent_account_access_key_key_vault_secret_name (alternative to agent_account_access_key - read from Key Vault instead)
     - agent_account_name
+    - agent_account_name_key_vault_id (alternative to agent_account_name - read from Key Vault instead)
+    - agent_account_name_key_vault_secret_name (alternative to agent_account_name - read from Key Vault instead)
     - controller_host_name
     - name
     - spring_cloud_service_id
@@ -18,18 +22,22 @@ Optional:
 EOT
 
   type = map(object({
-    agent_account_access_key = string
-    agent_account_name       = string
-    controller_host_name     = string
-    name                     = string
-    spring_cloud_service_id  = string
-    agent_application_name   = optional(string)
-    agent_node_name          = optional(string)
-    agent_tier_name          = optional(string)
-    agent_unique_host_id     = optional(string)
-    controller_port          = optional(number)
-    controller_ssl_enabled   = optional(bool)
-    globally_enabled         = optional(bool)
+    agent_account_access_key                       = string
+    agent_account_access_key_key_vault_id          = optional(string)
+    agent_account_access_key_key_vault_secret_name = optional(string)
+    agent_account_name                             = string
+    agent_account_name_key_vault_id                = optional(string)
+    agent_account_name_key_vault_secret_name       = optional(string)
+    controller_host_name                           = string
+    name                                           = string
+    spring_cloud_service_id                        = string
+    agent_application_name                         = optional(string)
+    agent_node_name                                = optional(string)
+    agent_tier_name                                = optional(string)
+    agent_unique_host_id                           = optional(string)
+    controller_port                                = optional(number)
+    controller_ssl_enabled                         = optional(bool)
+    globally_enabled                               = optional(bool)
   }))
 }
 
